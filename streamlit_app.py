45d65539395fc428d10495e19610d7fb29167643
Nickname3154 <rladudwl72@gmail.com>
2025-04-30 15:12:17 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,8 @@
 import streamlit as st
+from openai import OpenAI
 
+st.title("My LLM")
 user_key = st.text_input("Enter your API key")
 user_Q = st.text_input("write your question")
+
+client = OpenAI(api_key = user_key)
